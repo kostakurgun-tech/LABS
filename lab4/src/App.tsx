df04b968519e9d6f2f3d0d38a1c7cb2670d3de27
Minarc <kostakurgun@gmail.com>
2026-06-15 06:29:34 +0300

    Lab 4: Add NewTaskPage with task creation form

--- lab4/src/App.tsx
+++ lab4/src/App.tsx
@@ -5,7 +5,7 @@ import { INITIAL_TASKS } from "./data/initialTasks";
 import type { Task } from "./types/task";
 import TasksPage from "./pages/TasksPage/TasksPage";
 import TaskDetailPage from "./pages/TaskDetailPage/TaskDetailPage";
-// import NewTaskPage from "./pages/NewTaskPage/NewTaskPage";
+import NewTaskPage from "./pages/NewTaskPage/NewTaskPage";
 
 export default function App() {
   const [tasks, setTasks] = useState<Task[]>(INITIAL_TASKS);
@@ -22,11 +22,11 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Navigate to="/tasks" replace />} />
           <Route path="tasks" element={<TasksPage tasks={tasks} onDelete={deleteTask} />} />
+          <Route path="tasks/new" element={<NewTaskPage onAdd={addTask} />} />
           <Route
             path="tasks/:id"
             element={<TaskDetailPage tasks={tasks} onUpdate={updateTask} onDelete={deleteTask} />}
           />
-          {/* tasks/new додамо на Етапі 7 */}
         </Route>
       </Routes>
     </BrowserRouter>
